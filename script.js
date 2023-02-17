e16677008dc2870479b031a69fa0c7888650576b
Anton <anton20020426@gmail.com>
2023-02-17 19:36:32 +0100

    Check if password inputs are the same

--- script.js
+++ script.js
@@ -19,13 +19,44 @@ function showError() {
     if (password.validity.valueMissing) {
         passwordError.textContent = "You need to enter a password.";
     } else if (password.value.search(/[A-Z]/) === -1) {
-        passwordError.textContent = "You're missing an upper case";
+        passwordError.textContent = "You're missing an upper case.";
     } else if (password.value.search(/[a-z]/) === -1) {
-        passwordError.textContent = "You're missing a lower case";
+        passwordError.textContent = "You're missing a lower case.";
     } else if (password.value.search(/\d/) === -1) {
-        passwordError.textContent = "You're missing a digit";
+        passwordError.textContent = "You're missing a digit.";
     } else if (password.value.length < 8 ) {
-        passwordError.textContent = "Password has to be 8-20 characters"
+        passwordError.textContent = "Password has to be 8-20 characters."
     }
     passwordError.classList.add("password-error");
 };
+
+const confirmPassword = document.querySelector('#confirm-password');
+confirmPassword.addEventListener('input', checkPasswords);
+
+function checkPasswords(){
+    if (password.value !== confirmPassword.value && password.value != null) {
+        passwordError.classList.add("password-error");
+        passwordError.textContent = "Passwords do not match.";
+
+        confirmPassword.setCustomValidity("Password has to stay the same!");
+
+    } else {
+        passwordError.textContent = "";
+        passwordError.classList.remove("password-error");
+
+        confirmPassword.setCustomValidity("");
+    }
+};
+
+// inputs for custom messages
+// const firstName = document.querySelector("#first-name");
+
+// const submitButton = document.querySelector(".create-account>button");
+// submitButton.addEventListener('click', ()=> {
+
+//     if (!firstName.checkValidity()) {
+//         firstName.setCustomValidity("Format: only upper or lower case letters are allowed.");
+//     } else{
+//         firstName.setCustomValidity("");
+//     }
+// });
