cda11a936e8c743315c0b4d47a7a09c7055771c8
Anton <anton20020426@gmail.com>
2023-02-17 11:46:57 +0100

    Start working on a side branch

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// const form = document.querySelector("form");
-// const password = document.querySelector("#password");
-// const passwordError = document.querySelector
-// ('#password + span');
+const form = document.querySelector("form");
+const password = document.querySelector("#password");
+const passwordError = document.querySelector
+('#password + span');
 
-// password.addEventListener('input', (e) => {
-//     if (password.validity.valid) {
-//         passwordError.textContent = "";
-//     } else {
-//         showError();
-//     }
-// });
+password.addEventListener('input', (e) => {
+    if (password.validity.valid) {
+        passwordError.textContent = "";
+    } else {
+        showError();
+    }
+});
 
-// form.addEventListener("submit", (e) => {
-//     if (!password.validity.valid) {
-//         showError();
-//         event.preventDefault();
-//     }
-// });
+form.addEventListener("submit", (e) => {
+    if (!password.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+});
 
-// function showError() {
-//     if (password.validity.valueMissing) {
-//         passwordError.textContent = "You need to enter a password.";
-//     } else if (password.validity.patternMismatch) {
-//         passwordError.textContent = "Entered value needs to be of the proper format.";
-//     } else if (password.validity.tooShort) {
-//         passwordError.textContent = `Email should be at least ${password.minlength} characters; you entered ${password.value.length}.`;
-//     }
-// }
+function showError() {
+    if (password.validity.valueMissing) {
+        passwordError.textContent = "You need to enter a password.";
+    } else if (password.validity.patternMismatch) {
+        passwordError.textContent = "Entered value needs to be of the proper format.";
+    } else if (password.validity.tooShort) {
+        passwordError.textContent = `Email should be at least ${password.minlength} characters; you entered ${password.value.length}.`;
+    }
+}
 
