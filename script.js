a1304a9c272a31aa91fb493b64e1c9b8e0d834d1
Anton <anton20020426@gmail.com>
2023-02-18 11:03:39 +0100

    Show error messages to state the format of inputs

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const confirmPassword = document.querySelector('#confirm-password');
 confirmPassword.addEventListener('input', checkPasswords);
 
 function checkPasswords(){
-    if (password.value !== confirmPassword.value && password.value != null) {
+    if (password.value !== confirmPassword.value) {
         passwordError.classList.add("password-error");
         passwordError.textContent = "Passwords do not match.";
 
@@ -49,14 +49,90 @@ function checkPasswords(){
 };
 
 // inputs for custom messages
-// const firstName = document.querySelector("#first-name");
+const firstName = document.querySelector("#first-name");
+const firstNameSpan = document.querySelector("#first-name + span");
 
-// const submitButton = document.querySelector(".create-account>button");
-// submitButton.addEventListener('click', ()=> {
+const lastName = document.querySelector("#last-name");
+const lastNameSpan = document.querySelector("#last-name + span");
 
-//     if (!firstName.checkValidity()) {
-//         firstName.setCustomValidity("Format: only upper or lower case letters are allowed.");
-//     } else{
-//         firstName.setCustomValidity("");
-//     }
-// });
+
+firstName.addEventListener('input', () => {
+    if (firstName.validity.valid) {
+        firstNameSpan.textContent = "";
+        firstNameSpan.classList.remove("password-error");
+    } else {
+        showFirstNameError();
+    }
+});
+
+lastName.addEventListener('input', ()=> {
+    if (lastName.validity.valid) {
+        lastNameSpan.textContent = "";
+        lastNameSpan.classList.remove("password-error");
+    } else {
+        showLastNameError();
+    }
+});
+
+function showFirstNameError(){
+    if (firstName.validity.patternMismatch) {
+        firstNameSpan.textContent = "Format: only upper and lower case letters are allowed.";
+    } else if (firstName.validity.valueMissing) {
+        firstNameSpan.textContent = "You need to enter a name.";
+    }
+    firstNameSpan.classList.add("password-error");
+};
+
+function showLastNameError(){
+    if(lastName.validity.patternMismatch) {
+        lastNameSpan.textContent = "Format: only upper and lower case letters are allowed.";
+    } else if (lastName.validity.valueMissing) {
+        lastNameSpan.textContent = "You need to enter a name.";
+    }
+    lastNameSpan.classList.add("password-error");
+};
+
+
+const email = document.querySelector('#email');
+const emailSpan = document.querySelector("#email + span");
+
+email.addEventListener('input', ()=> {
+    if (email.validity.valid) {
+        emailSpan.textContent = "";
+        emailSpan.classList.remove("password-error");
+    } else {
+        showEmailError();
+    }
+});
+
+function showEmailError() {
+    if (email.validity.patternMismatch) {
+        emailSpan.innerHTML = `Format: a local-part, the symbol @, and a domain.
+        <br>
+        Example: john@gmail.com`;
+    } else if (email.validity.valueMissing) {
+        emailSpan.textContent = 'You need to enter an email.';
+    }
+    emailSpan.classList.add('password-error');
+};
+
+const phoneNumber = document.querySelector('#number');
+const phoneNumberSpan = document.querySelector("#number + span");
+
+phoneNumber.addEventListener('input', ()=> {
+    if (phoneNumber.validity.valid) {
+        phoneNumberSpan.textContent = "";
+        phoneNumberSpan.classList.remove('password-error');
+    } else {
+        showPhoneNumberError();
+    }
+});
+
+function showPhoneNumberError() {
+    if (phoneNumber.validity.patternMismatch) {
+        phoneNumberSpan.innerHTML = `Format: 12 digits that can be separated with space or dash.`;
+    } else if (phoneNumber.validity.valueMissing) {
+        phoneNumberSpan.textContent = "You need to enter a number.";
+    }
+    phoneNumberSpan.classList.add('password-error');
+};
